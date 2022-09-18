4ca742fa4d5b2fa7a7f1469b2c1080a9de774586
yunior <yunior@web724.com>
2022-09-18 18:38:44 -0300

    Validación que incluye chequeo si se pudo establecer comunicación con la base de datos

--- src/seed/user.seed.ts
+++ src/seed/user.seed.ts
@@ -1,4 +1,3 @@
-import UserModel from "../models/user.schema";
 import logger from '../utils/logger.utils';
 import usersData from './user.data';
 import connectDataBase from '../db/mongo';
@@ -6,16 +5,21 @@ import { getUserByEmail, bulkUsersDB } from '../services/user.service';
 
 const seedUsers = async () => {
     try{
-        await connectDataBase();
-        let newUsersDataInsert = [];
-        for (let index = 0; index < usersData.length; index++) {
-            const findUserByEmail = await getUserByEmail(usersData[index].email);
-            if(!findUserByEmail) newUsersDataInsert.push(usersData[index]);
-        };
-        if(newUsersDataInsert.length > 0){
-            const saveUsers = await bulkUsersDB(newUsersDataInsert);
-            return saveUsers;
-        }else {
+        const connect = await connectDataBase();
+        if(connect){
+            let newUsersDataInsert = [];
+            for (let index = 0; index < usersData.length; index++) {
+                const findUserByEmail = await getUserByEmail(usersData[index].email);
+                if(!findUserByEmail) newUsersDataInsert.push(usersData[index]);
+            };
+            if(newUsersDataInsert.length > 0){
+                const saveUsers = await bulkUsersDB(newUsersDataInsert);
+                return saveUsers;
+            }else {
+                return null;
+            }
+        }else{
+            logger.info('No se pudo establecer conexión con la base de datos.');
             return null;
         }
     }catch(err){
